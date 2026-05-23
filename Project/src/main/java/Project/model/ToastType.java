189e6bc31ea4396b1572bb5e111fff3bd0f6f4d1
guguluP <88131247+guguluP@users.noreply.github.com>
2026-05-23 18:12:47 +0530

    Delete Project directory

--- Project/src/main/java/Project/model/ToastType.java
+++
@@ -1,9 +0,0 @@
-package Project.model;
-
-/**
- * Types for the advanced toast notifications in the UI.
- * Extracted from GlassCalculator to allow reuse and cleaner main class.
- */
-public enum ToastType {
-    SUCCESS, ERROR, INFO
-}
